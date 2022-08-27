7f3c5c0b44e7bb39ab85b4ffcd80ee345a875fec
klaus <klaus@backend.shemhazai.de>
2022-08-27 18:05:08 +0200

    add: Custom Logger

--- scripts/shelly_mqtt_subscriber/get_shelly_data.py
+++ scripts/shelly_mqtt_subscriber/get_shelly_data.py
@@ -9,6 +9,35 @@ import os
 import sys
 import pymysql
 import hashlib
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
+
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.grey + self.fmt + self.reset,
+            logging.INFO: self.blue + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.bold_red + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 
 
 class ShellyDataset:
@@ -44,9 +73,11 @@ class ShellyDataset:
 
     def addDataToDB(self):
         if (db):
-            print("Datenbankverbindung OK")
+            logger.info("Datenbankverbindung OK")
         else:
-            print("Datenbankversbingung FEHLER")
+            logger.critical("Datenbankversbingung FEHLER")
+            sys.exit(0)
+
         sql = "INSERT INTO common_data (device, type, event, reading, json_data, unit) VALUES (%s, %s, %s, %s, %s, %s)"
         cursor.execute(sql,(dev, dev_type, "update", "data", json.dumps(self.data), "json" ))
         db.commit()
@@ -75,6 +106,28 @@ def on_relay(client, userdata, message):
     # print ("relay Message received: "  + str(m_decode))
 
 os.chdir(os.path.dirname(__file__))
+
+# Create custom logger logging all five levels
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Define format for logs
+fmt = '%(asctime)s | %(levelname)8s | %(message)s'
+
+# Create stdout handler for logging to the console (logs all five levels)
+stdout_handler = logging.StreamHandler()
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(CustomFormatter(fmt))
+
+# Create file handler for logging to a file (logs all five levels)
+#today = datetime.date.today()
+#file_handler = logging.FileHandler('my_app_{}.log'.format(today.strftime('%Y_%m_%d')))
+#file_handler.setLevel(logging.DEBUG)
+#file_handler.setFormatter(logging.Formatter(fmt))
+
+# Add both handlers to the logger
+logger.addHandler(stdout_handler)
+#logger.addHandler(file_handler)
 
 try:
     with open(os.getcwd() + "/config.json") as json_config_file:
