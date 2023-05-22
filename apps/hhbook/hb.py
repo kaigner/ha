7bdcf4472e364baa73b6c0eb8c91cf44f09bc767
Klaus Aigner <klaus@shodan-2.fritz.box>
2023-05-22 20:04:56 +0200

    add: Datenbankpart und Klassen aus dem alten Script

--- apps/hhbook/hb.py
+++ apps/hhbook/hb.py
@@ -1,17 +1,122 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-import sys
+import json
+import pymysql
 import pytz
+import os
+import sys
+
+
+# Lese Konfig 
+os.chdir(os.path.dirname(__file__))
+
+# Read Config File
+with open(os.getcwd() + "/config.json") as json_config_file:
+	config_data=json.load(json_config_file)
+db_host = config_data['mysql']['host']
+db_user = config_data['mysql']['user']
+db_pass = config_data['mysql']['pass']
+db_name = config_data['mysql']['db']
+
+dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Connect to Database
+try:
+	db = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+except pymysql.err.OperationalError as error:
+	print("- error connecting to database: {}".format(error))
+	pass
+
+cursor = db.cursor()
+if (db):
+	print("Datenbankverbindung OK")
+	# pass #print("Datenbankverbindung OK")
+else:
+	print("konnte keine Verbindung zur Datenbank herstellen")
+	sys.exit(9)
 
 class receipt:
     def __init__(self):
         self.date = datetime.now(pytz.timezone('Europe/Berlin'))
+        self.purchase_nr = 0
         self.store = 0
         self.article = ""
+        self.single_price = 0
+        self.amount = 0
+        self.total_price = 0
+        self.single_weight = 0
+        self.total_weight = 0
+        self.store_flag_0 = ""
+        self.my_category = ""
 
 
-        pass
+class Store():
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.alias = ""
+        self.street = ""
+        self.housenumber = ""
+        self.zip = 0
+        self.town = ""
+        self.state = "Bayern"
+        self.country = "Deutschland"
+
+
+class Ledger():
+    def __init__(self):
+        self.account = {"name": None, "iban": None}
+
+    def readAll():
+        try:
+            with db.cursor() as cur:
+                cur.execute("SELECT id, name, comment, iban FROM ledger")
+                rows = cur.fetchall()
+        finally:
+            cur.close()
+
+    def list(self, ask, id):
+        try:
+            with db.cursor() as cur:
+                if id == 0:
+                    cur.execute("SELECT id, name, comment, iban FROM ledger")
+                    rows = cur.fetchall()
+                else:
+                    cur.execute(
+                        "SELECT id, name, comment, iban FROM ledger where id = %s", (id,))
+                    rows = cur.fetchall()
+
+                printHeader()
+                print("[green]Konto Auswahl:[/green]\n")
+                for row in rows:
+                    print("{} -> {}".format(row[0], row[1]))
+        finally:
+            cur.close()
+
+        if ask == "yes":
+            x = input("Enter für weiter")
+
+    def choose(self):
+        print("\n[magenta bold]Auswahl:[/magenta bold]", end="")
+        x = input(" ")
+        if x == "":
+            x = 1
+        # simple Eingebaprüfung / SQL Injection Check - NACHARBEITEN
+        try:
+            val = int(x)
+        except ValueError:
+            print("Keine Zahl, Abbruch")
+            sys.exit(1)
+
+        try:
+            with db.cursor() as cur:
+                cur.execute("SELECT id, name FROM ledger WHERE id = %s", (x,))
+                row = cur.fetchone()
+                purchase.ledger["id"] = int(row[0])
+                purchase.ledger["name"] = row[1]
+        finally:
+            cur.close()
 
 
 print(sys.version_info)
