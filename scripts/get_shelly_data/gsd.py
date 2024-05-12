048f3d4dea5d22affbb2709907560a565997d73c
Klaus <kaigner@shemhazai.de>
2024-05-12 16:34:38 +0200

    Debug Print entfernt

--- scripts/get_shelly_data/gsd.py
+++ scripts/get_shelly_data/gsd.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-import time
 import json
-import pymysql
-from requests.auth import HTTPBasicAuth
-import requests
 import os
 import sys
+import time
+from datetime import datetime
 
+import pymysql
+import requests
+from requests.auth import HTTPBasicAuth
 
-
-# Lese Konfig 
+# Lese Konfig
 os.chdir(os.path.dirname(__file__))
 
 # Read Config File
 with open(os.getcwd() + "/config.json") as json_config_file:
-        config_data=json.load(json_config_file)
-shelly_user  = config_data['http_auth']['user']
-shelly_pass  = config_data['http_auth']['pass']
-db_host  = config_data['mysql']['host']
-db_user  = config_data['mysql']['user']
-db_pass  = config_data['mysql']['pass']
-db_name  = config_data['mysql']['db']
+    config_data = json.load(json_config_file)
+shelly_user = config_data["http_auth"]["user"]
+shelly_pass = config_data["http_auth"]["pass"]
+db_host = config_data["mysql"]["host"]
+db_user = config_data["mysql"]["user"]
+db_pass = config_data["mysql"]["pass"]
+db_name = config_data["mysql"]["db"]
 
 
-shellys = {"192.168.2.21":"shelly_plug_s_spuelmaschine", "192.168.2.22":"shelly_plug_s_ap_klaus", "192.168.2.23":"shelly_plug_s_ap_claudia", "192.168.2.24":"shelly_plug_s_bad"}
+shellys = {
+    "192.168.2.21": "shelly_plug_s_spuelmaschine",
+    "192.168.2.22": "shelly_plug_s_ap_klaus",
+    "192.168.2.23": "shelly_plug_s_ap_claudia",
+    "192.168.2.24": "shelly_plug_s_bad",
+}
 
 
 def get_status_in_json(ipaddr):
     try:
-        response = requests.get('http://' + ipaddr + '/status', auth=HTTPBasicAuth(shelly_user, shelly_pass))
-        return(response.json())
+        response = requests.get(
+            "http://" + ipaddr + "/status", auth=HTTPBasicAuth(shelly_user, shelly_pass)
+        )
+        return response.json()
     except:
         print("Kann shelly mit der ip: {} nicht erreichen".format(ipaddr))
 
@@ -40,7 +46,9 @@ def insert_json_data_to_database(json_data, aliasname):
     device = json_data["mac"]
     vendor = "Shelly"
     try:
-        db = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+        db = pymysql.connect(
+            host=db_host, user=db_user, password=db_pass, database=db_name
+        )
         cursor = db.cursor()
         sql = "INSERT INTO common_data (timestamp, device, type, json_data, unit) VALUES (%s, %s, %s, %s, %s)"
         cursor.execute(sql, (dt, aliasname, vendor, json.dumps(json_data), "json"))
@@ -57,23 +65,18 @@ def parse_json_and_do_the_rest_because_i_have_no_class(json_data):
 
 
 if __name__ == "__main__":
-    while True: 
-        print("Konfigurierte Shellys:")
+    while True:
+        # print("Konfigurierte Shellys:")
         dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         for key, value in shellys.items():
-            ipaddr    = key
+            ipaddr = key
             aliasname = value
             json_data = get_status_in_json(ipaddr)
             if json_data is not None:
-                parse_json_and_do_the_rest_because_i_have_no_class(json_data)
+                # parse_json_and_do_the_rest_because_i_have_no_class(json_data)
                 insert_json_data_to_database(json_data, aliasname)
             json_data = {}
         time.sleep(60)
 
 
 sys.exit(0)
-
-
-
-
-
